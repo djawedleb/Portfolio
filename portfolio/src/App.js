b999d57c00bceeb8476071f4fce0594acdfd6930
Djaweddd <djawedlebaili156@gmail.com>
2024-10-01 22:14:30 +0100

    navbar done

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Header from './components/Header';
+
 
 function App() {
   return (
-    
+    <Header></Header>
+  )
 }
 
 export default App;
